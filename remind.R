4813706570680ad16d2fbda8ce3abda6835b72a5
vipasha13033549 <123747328+vipasha13033549@users.noreply.github.com>
2023-01-27 15:25:04 +0100

    Add files via upload

---
+++ remind.R
@@ -0,0 +1,8 @@
+remind_me <- function() {
+  errands <- c("pay rent", "file taxes", "get milk", "get flour")
+  print(errands)
+  call <- c("Ash", "Mona", "Mom", "Dad", "Gargi", "Stephen")
+  print(call)
+  
+}
+remind_me()
